e9deaaa9dec75c0facf9de5ae17afdeb8e7bf345
xabibullayev <xabibullayevmm@gmail.com>
2023-05-19 15:59:32 +0500

    another try

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -74,7 +74,11 @@ export const login = async (req: Request, res: Response) => {
     );
 
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie("access_token", token, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      })
       .status(200)
       .json("Logged in succesfully!");
   } catch (err) {
